960c6c3d3865774e79a590ff285358371706c3fe
Otávio Almeida <otavioalmeida650@gmail.com>
2023-05-24 16:05:17 -0300

    gauss?

--- convolucao_laplaciano/dashboard/Convolucao.py
+++ convolucao_laplaciano/dashboard/Convolucao.py
@@ -3,6 +3,7 @@ from streamlit_toggle import st_toggle_switch as toggle
 import numpy as np
 from PIL import Image
 from multiprocessing import Pool
+from math import pi, e
 
 st.title("Convolução Laplaciano")
 
@@ -19,51 +20,121 @@ st.markdown("#### Kernel")
 col_1, col_2 = st.columns(2)
 
 with col_2:
-    size = st.number_input(
-        "Tamanho Kernel", min_value=3, max_value=None, value=3, step=2
+    size = int(
+        st.number_input("Tamanho Kernel", min_value=3, max_value=None, value=3, step=2)
     )
-    is_const = toggle("Constante")
+    padding = (size - 1) // 2
+    is_const = toggle("Constante", label_after=True)
 
-divisor = 9 if is_const else (int(size) ** 2)
-kernel = np.ones((int(size), int(size))) / divisor
+divisor = 9 if is_const else (size**2)
+kernel = np.ones((size, size)) / divisor
 
 with col_1:
     st.write(kernel)
 
 
 image_array = np.array(image)
-image_arrayb = np.pad(image_array, ((int(size) - 1) // 2), "constant")
+image_arrayb = np.pad(image_array, padding, "edge")
 
 image_array_conv = image_array.copy()
 
 
-def f(i: int, padding: int, image_arrayb: np.ndarray):
+def f(i: int, padding: int, image_arrayb: np.ndarray, kernel: np.ndarray):
     pixels = list()
-    for j, _ in enumerate(image_arrayb[i][padding:-padding]):
+    for j in range(padding, len(image_arrayb[i]) - padding):
         soma = 0
-        for desloc_i in range(3):
-            for desloc_j in range(3):
+        for desloc_i in range(len(kernel)):
+            for desloc_j in range(len(kernel[desloc_i])):
                 soma += (
                     kernel[desloc_i][desloc_j]
-                    * image_arrayb[i - 1 + desloc_i][j - 1 + desloc_j]
+                    * image_arrayb[i - padding + desloc_i][j - padding + desloc_j]
+                    # 466 - 1 + 4
                 )
         pixels.append(soma)
     return np.array(pixels)
 
 
+prog_bar_lua = st.progress(0, text="Conv Lua")
 image_array_conv = list()
-for i, _ in enumerate(image_arrayb[((int(size) - 1) // 2) : -((int(size) - 1) // 2)]):
-    result = f(i + ((int(size) - 1) // 2), ((int(size) - 1) // 2), image_arrayb)
-    result = np.array(result)
+for i in range(padding, len(image_arrayb) - padding):
+    # 464 + 2
+    prog_bar_lua.progress(((i - padding) / (len(image_arrayb) - padding)))
+    result = f(i, padding, image_arrayb, kernel)
     image_array_conv.append(result)
 image_array_conv = np.array(image_array_conv)
 
 image_array_conv_min = image_array_conv - np.min(image_array_conv)
 image_array_conv = np.rint(255 * (image_array_conv_min / np.max(image_array_conv_min)))
 
+# st.table(image_array_conv)
+
 image_final_conv = Image.fromarray(image_array_conv)
 
 st.image(image_final_conv.convert("L"))
+
+
+# region gaussiano
+
+
+def gauss(x, y, sigma: float):
+    return 1 / (2 * pi * sigma**2) * e ** (-(x**2 + y**2) / (2 * sigma**2))
+
+
+col_1, col_2 = st.columns(2)
+
+with col_2:
+    size = int(
+        st.number_input(
+            "Tamanho Kernel Gauss", min_value=3, max_value=None, value=3, step=2
+        )
+    )
+    sigma = float(
+        st.number_input("Alpha", min_value=0.1, max_value=1.0, value=0.6, step=0.01)
+    )
+    padding = (size - 1) // 2
+
+grid_lin_space = np.linspace(
+    -((size - 1) // 2), ((size - 1) // 2), size, dtype=np.int64
+)
+# st.write(grid_lin_space)
+kernel_x, kernel_y = np.meshgrid(grid_lin_space, grid_lin_space)
+# st.table(kernel_y)
+
+
+kernel = gauss(
+    kernel_x,
+    kernel_y,
+    sigma,
+)
+
+with col_1:
+    st.write(kernel)
+
+image_array_conv_gauss = image_array.copy()
+
+
+prog_bar_lua_gauss = st.progress(0, text="Conv Lua Gauss")
+image_array_conv_gauss = list()
+for i in range(padding, len(image_arrayb) - padding):
+    # 464 + 2
+    prog_bar_lua_gauss.progress(((i - padding) / (len(image_arrayb) - padding)))
+    result = f(i, padding, image_arrayb, kernel)
+    result = np.array(result)
+    image_array_conv_gauss.append(result)
+image_array_conv_gauss = np.array(image_array_conv_gauss)
+
+image_array_conv_min = image_array_conv_gauss - np.min(image_array_conv_gauss)
+image_array_conv_gauss = np.rint(
+    255 * (image_array_conv_min / np.max(image_array_conv_min))
+)
+
+# st.table(image_array_conv)
+
+image_final_conv = Image.fromarray(image_array_conv_gauss)
+
+st.image(image_final_conv.convert("L"))
+
+# endregion
 
 image = Image.open("./data/11_test.png")
 
@@ -104,25 +175,27 @@ image_arrayb = np.pad(image_array, 1, "constant")
 
 image_array_conv = image_array.copy()
 
+prog_bar_eye = st.progress(0, "Conv Eye")
 image_array_conv = list()
 for i, _ in enumerate(image_arrayb[1:-1]):
-    result = f(i + 1, 1, image_arrayb)
+    prog_bar_eye.progress(i / len(image_arrayb[1:-1]))
+    result = f(i + 1, 1, image_arrayb, kernel)
     result = np.array(result)
     image_array_conv.append(result)
 image_array_conv = np.array(image_array_conv)
 
+image_final_array = image_array + image_array_conv
+
 image_array_conv_min = image_array_conv - np.min(image_array_conv)
 image_array_conv = np.rint(255 * (image_array_conv_min / np.max(image_array_conv_min)))
-
-image_final_array = image_array + image_array_conv
 
 image_final_array_min = image_final_array - np.min(image_final_array)
 image_final_array = np.rint(
     255 * (image_final_array_min / np.max(image_final_array_min))
 )
 
-st.write(np.max(image_final_array))
-st.write(np.min(image_final_array))
+# st.write(np.max(image_final_array))
+# st.write(np.min(image_final_array))
 
 image_final_conv = Image.fromarray(image_array_conv)
 image_final = Image.fromarray(image_final_array)
